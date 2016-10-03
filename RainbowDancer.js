4e04edf6576bcb059516f37421f5ced1941ea3bb
David Walsh <dawalsh@gmail.com>
2016-10-03 13:20:34 -0700

    Import files

---
+++ RainbowDancer.js
@@ -0,0 +1,101 @@
+var instantaneousRadius;
+
+var rand = function (min, max) {
+  return Math.random() * (max - min) + min;
+};
+
+var randInt = function(min, max) {
+  return Math.floor(min + Math.random() * (max - min + 1));
+};
+
+var RainbowCircle = function() {
+  this.damper = 16; // easing in x,y movement; 1 to 20 work well
+  this.x = width / 2, + 1; // start in center of screen
+  this.y = height / 2 + 1; // start in center of screen
+  this.targetX = this.x;
+  this.targetY = this.y;
+  this.angle = 0; // initial value
+  this.distance = 0; // inital value
+  
+
+  this.path = [];
+  this.count = 40;//randInt(10, 60);
+  this.hue = 0;
+  this.radius = 70;
+  this.theta = rand(0, Math.PI * 2); // angle of new paths relative to center
+  this.thetaSpeed = Math.PI / 20; // speed of the 'sweep'
+  this.clockwise = 1; // -1 for counterclockwise
+
+  this.updateAngleAndDistance();
+
+};
+
+RainbowCircle.prototype.draw = function(i) {
+  ctx.beginPath();
+  var rando = rand(1, 7);
+  for (var j = 0, length = this.path.length; j < length; j++ ) {
+    ctx[(j === 0) ? 'moveTo' : 'lineTo' ](this.path[j].x + rand(-rando, rando), this.path[j].y + rand(-rando, rando));
+  }
+  ctx.strokeStyle = 'hsla(' + rand(this.hue, this.hue + 10) + ', 45%, 25%, .6)';
+  ctx.lineWidth = rand(0.1, 4);
+  ctx.stroke();
+
+  // debugging angle
+  // ctx.beginPath();
+  // ctx.moveTo(this.x, this.y);
+  // ctx.lineTo(this.x + this.radius * Math.cos(this.theta), this.y + this.radius * Math.sin(this.theta));
+  // ctx.lineWidth = 2;
+  // ctx.strokeStyle = 'hsla(0, 70%, 50%, 1)';
+  // ctx.stroke();
+
+  // // debugging angle
+  // ctx.beginPath();
+  // ctx.moveTo(this.x, this.y);
+  // ctx.lineTo(this.x + this.radius * Math.cos(this.angle), this.y + this.radius * Math.sin(this.angle));
+  // ctx.lineWidth = 2;
+  // ctx.strokeStyle = 'hsla(200, 70%, 50%, 1)';
+  // ctx.stroke();
+};
+
+RainbowCircle.prototype.step = function(i) {
+  this.updateAngleAndDistance();
+
+  this.x = this.x + Math.cos(this.angle) * this.distance / this.damper;
+  this.y = this.y + Math.sin(this.angle) * this.distance / this.damper;
+  
+  var diffAngle = Math.abs((this.angle - this.theta));
+  
+  if (this.distance < this.radius || Math.abs(diffAngle - Math.PI) > Math.PI / 6) {
+    this.theta = this.theta > Math.PI ? this.theta - Math.PI * 2 : this.theta;
+
+    this.theta = ((this.theta + this.thetaSpeed * this.clockwise) % (2 * Math.PI));
+  }
+
+  // make the orb to 'breathe'
+  instantaneousRadius = this.radius + Math.cos(tick / 20 % (Math.PI * 2)) * this.radius / 6;
+
+  
+  this.path.push({
+    x: this.x + instantaneousRadius / (this.distance / instantaneousRadius * .25 + 1) * Math.cos(this.theta),
+    y: this.y + instantaneousRadius / (this.distance / instantaneousRadius * .25 + 1) * Math.sin(this.theta)
+  });
+
+
+  this.hue = tick / 2 % 360;
+
+  if (this.path.length > this.count) {
+    this.path.shift();
+  }
+};
+
+RainbowCircle.prototype.moveTo = function(x, y) {
+  this.targetX = x;
+  this.targetY = y;
+};
+
+RainbowCircle.prototype.updateAngleAndDistance = function() {
+  var dx = this.targetX - this.x;
+  var dy = this.targetY - this.y;
+  this.distance = Math.sqrt(dx * dx + dy * dy);
+  this.angle = Math.atan2(dy, dx);
+};
---
+++ app.js
@@ -0,0 +1,109 @@
+var canvas,
+  ctx,
+  width,
+  height,
+  size,
+  circles,
+  tick;
+
+
+
+var reset = function() {
+  width = Math.ceil(window.innerWidth / 2) * 2;
+  height = Math.ceil(window.innerHeight / 2) * 2;
+  tick = 0;
+  
+  circles.length = 0; // reset circles
+  canvas.width = width;
+  canvas.height = height;
+};
+
+
+// var create = function () {
+//   if (tick % 10 === 0) {   
+//     circles.push(new RainbowCircle());
+//   }
+// };
+
+var create = function() {
+  if (circles.length < 1) {
+    circles.push(new RainbowCircle());
+  }
+};
+
+var draw = function() {
+  //ctx.save();
+  //ctx.translate( width / 2, height / 2 );
+  //ctx.rotate( tick * 0.001 );
+  //var scale = 0.8 + Math.cos( tick * 0.02 ) * 0.2;
+  //ctx.scale( scale, scale );
+  //ctx.translate( -width / 2, -height / 2 );
+  var i = circles.length;
+  while (i--) {
+    circles[i].draw(i); 
+  }
+  //ctx.restore();
+};
+
+var step = function () {
+  var i = circles.length;
+  while (i--) {
+    circles[i].step(i); 
+  }
+};
+
+var clear = function() {
+  ctx.globalCompositeOperation = 'destination-out';
+  ctx.fillStyle = 'hsla(0, 0%, 0%, 0.1';
+  ctx.fillRect(0, 0, width, height);
+  ctx.globalCompositeOperation = 'lighter';
+};
+
+var loop = function() {
+  requestAnimationFrame(loop);
+  create();
+  step();
+  clear();
+  draw();
+  tick++;
+};
+
+var updateCircles = function(e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  var i = circles.length;
+  while (i--) {
+    circles[i].moveTo(x, y);
+  }
+};
+
+var resetCircles = function(e) {
+  var x = window.innerWidth / 2 + 1;
+  var y = window.innerHeight / 2 + 1;
+  var i = circles.length;
+  while (i--) {
+    circles[i].moveTo(x, y);
+  }
+};
+
+var init = function() {
+  canvas = document.getElementById('canvas');
+  canvas.addEventListener('mousemove', updateCircles);
+  canvas.addEventListener('mouseout', resetCircles);
+  ctx = canvas.getContext('2d');
+  size = 30;
+  circles = [];
+  reset();
+  loop();
+};
+
+init(); // kick it off
+
+
+/*
+var onresize = function () {
+  reset();  
+};
+
+window.addEventListener( 'resize', onresize );
+*/
